04f35aa2655952164a94e8beccdac726bf273a1c
Kyle Davidson <kyle.d@live.co.uk>
2023-06-19 20:52:26 +0100

    Initial project

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="hero my-auto">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Tunesday</h1>
+            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
